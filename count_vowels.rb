8bd3e5b447334ebb79f3a5d58ebe8eaf6f649582
valentinerutto <rutto28@gmail.com>
2015-08-11 18:55:39 +0300

    count_vowels method using string.count method

---
+++ count_vowels.rb
@@ -0,0 +1,8 @@
+def count_vowels(string)
+    return string.count( "aeiou") 
+end
+
+puts('count_vowels("abcd") == 1: ' + (count_vowels('abcd') == 1).to_s)
+puts('count_vowels("color") == 2: ' + (count_vowels('color') == 2).to_s)
+puts('count_vowels("colour") == 3: ' + (count_vowels('colour') == 3).to_s)
+puts('count_vowels("cecilia") == 4: ' + (count_vowels('cecilia') == 4).to_s)
