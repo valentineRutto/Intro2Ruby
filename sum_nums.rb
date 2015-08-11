f8856e6ed244c62859e09b48b9c264eb81921c61
valentinerutto <rutto28@gmail.com>
2015-08-11 17:42:07 +0300

    sum_num method with while loop

---
+++ sum_nums.rb
@@ -0,0 +1,16 @@
+def sum_nums(num)
+    sum=0
+    i=0
+    while i<=num
+        sum+=i
+        i+=1
+end
+      return sum
+  
+end
+
+puts('sum_nums(1) == 1: ' + (sum_nums(1) == 1).to_s)
+puts('sum_nums(2) == 3: ' + (sum_nums(2) == 3).to_s)
+puts('sum_nums(3) == 6: ' + (sum_nums(3) == 6).to_s)
+puts('sum_nums(4) == 10: ' + (sum_nums(4) == 10).to_s)
+puts('sum_nums(5) == 15: ' + (sum_nums(5) == 15).to_s)
