a205a93ce65be901077ba879e2b22d0dc314caa9
valentinerutto <rutto28@gmail.com>
2015-08-11 08:57:09 +0300

    string methods

---
+++ arrays.rb
@@ -0,0 +1,23 @@
+/////ARRAYS///
+arr=[]
+item=gets.chomp
+arr.push(item)
+//adds item to the end of the array 
+    arr.pop
+//removes item from the end of the array
+arr.shift
+//removes from the start
+arr.unshift
+//adds to the start
+["good","morning","madame"].join(" ")
+//["good morning madame]
+
+/////STRINGS//////
+str="my name is rose"
+str.length
+//outputs 15
+str.split(" ")
+//["my" "name" "is" "rose]
+["my" "name" "is" "rose].join(" ")
+//outputs ["my name is rose"]
+
