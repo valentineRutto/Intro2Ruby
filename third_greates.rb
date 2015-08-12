cf8b82c813e38585708838b9d99ffdbed3cba401
valentinerutto <rutto28@gmail.com>
2015-08-12 13:10:23 +0300

    initialcommit of third_greatestfile

---
+++ third_greates.rb
@@ -0,0 +1,12 @@
+def third_greatest(nums)
+    
+end
+
+puts(
+  'third_greatest([5, 3, 7]) == 3: ' +
+  (third_greatest([5, 3, 7]) == 3).to_s
+)
+puts(
+  'third_greatest([5, 3, 7, 4]) == 4: ' +
+  (third_greatest([5, 3, 7, 4]) == 4).to_s
+)
