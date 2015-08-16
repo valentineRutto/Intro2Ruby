6324b975cce502e8b67c51c662d04360c8f8e985
valentinerutto <rutto28@gmail.com>
2015-08-16 15:59:00 +0300

    gcd method used gcd method

---
+++ GCD.rb
@@ -0,0 +1,17 @@
+def greatest_common_factor(number1, number2)
+    return number1.gcd(number2)
+end
+
+
+puts(
+  'greatest_common_factor(3, 9) == 3: ' +
+  (greatest_common_factor(3, 9) == 3).to_s
+)
+puts(
+  'greatest_common_factor(16, 24) == 8: ' +
+  (greatest_common_factor(16, 24) == 8).to_s
+)
+puts(
+  'greatest_common_factor(3, 5) == 1: ' +
+  (greatest_common_factor(3, 5) == 1).to_s
+)
